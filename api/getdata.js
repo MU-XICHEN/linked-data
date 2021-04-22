f341081360bdc9019789a783cd713871ffc3f3de
Ryan <965450326@qq.com>
2021-04-22 21:53:32 +0800

    chj :build dist

--- api/getdata.js
+++ api/getdata.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 let getData = function(url){
     return new Promise(resolve => {
+        console.log(url);
         axios.get(url)
             .then(({data}) => {
                 resolve(data)
